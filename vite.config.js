98446071be9d854ea0871dff870d60ee66b5b8be
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2024-04-12 12:21:00 +0000

    updated base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/teritech",
+    base: "/teritech-dev",
     build: {
         rollupOptions: {
             input: {
